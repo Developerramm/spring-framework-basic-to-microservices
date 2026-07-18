c42dc5157aaeb659057b1e86e6df2b2963c9d411
ram kumar <rr697144@gmail.com>
2026-07-18 09:31:57 +0530

    jdbc setup

---
+++ jdbc/Proj1_jdbc/src/com/app/jdbc/test/Test.java
@@ -0,0 +1,12 @@
+package com.app.jdbc.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Ram kumar ");
+		System.out.println("Welcome to jdbc tutorial here ");
+
+	}
+
+}
