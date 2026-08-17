8cdd93a9198e27d105f395127774c6cdf415681f
ram kumar <rr697144@gmail.com>
2026-08-17 06:59:34 +0530

    css style in react js

--- reactjs/firstapp/src/Bye.jsx
+++ reactjs/firstapp/src/Bye.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ram from "./Bye.module.css"
+
 
 const Bye = () => {
+ 
   return (
     <div>
-      <h3>This is bye component here </h3>
+      <h3 className='text'>This is bye component here </h3>
+
+      <p className={ram.box}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eaque omnis modi ex odio eum dolorum minima voluptatibus quasi ullam.</p>
+
+      <div className={`${ram.bg} ${ram.dabba}`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti vero id accusantium consequatur, nisi sequi molestias tenetur ratione quibusdam iusto aut modi hic dolorum eaque commodi. Porro, doloribus saepe!
+      </div>
     </div>
   )
 }
--- reactjs/firstapp/src/Hello.jsx
+++ reactjs/firstapp/src/Hello.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Hello = () => {
-  const name = "Ram kumar"
+  const name = "Ram kumar";
+
+  const headingStyle = {
+    color: "red",
+    textAlign: "center",
+    backgroundColor: "pink",
+    fontSize: 20,
+  };
+
   return (
     <div>
-      <h1>This is hello component </h1>
-      <h3 className='bg'>My name is {name} </h3>
+      <h1 style={headingStyle}>This is hello component </h1>
+      <h3
+        style={{ color: "red", textAlign: "center", fontFamily: "monospace" }}
+      >
+        My name is {name}{" "}
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Hello
+export default Hello;
