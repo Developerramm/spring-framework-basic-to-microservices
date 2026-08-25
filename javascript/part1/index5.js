5b8f68b013a8d9f0d0afdf9d1eaedeb8eea1c022
ram kumar <rr697144@gmail.com>
2026-08-25 07:20:51 +0530

    javascript practical

---
+++ javascript/part1/index5.js
@@ -0,0 +1,16 @@
+console.log("javascript practice");
+
+let age = 20;
+
+let state = "Bihar";
+
+if(age > 18){
+    console.log("You can vote here ")
+}
+
+if(state == "Bihar"){
+    console.log("You are Bihari");
+    if(age >= 20){
+        console.log("You can work in my company")
+    }
+}
