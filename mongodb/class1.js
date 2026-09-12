23a249608e455829782a7bc94dee807a0aa889a9
ram kumar <rr697144@gmail.com>
2026-09-12 12:01:57 +0530

    mongodb class1 and react state setup

---
+++ mongodb/class1.js
@@ -0,0 +1,22 @@
+let db = {};
+
+
+db.createCollection("school");
+
+db.createCollection("students");
+
+// show dbs 
+// show collections 
+
+// use school 
+
+db.students.renameCollection("std");
+
+db.help()
+
+db.collection_name.help()
+
+db.library.drop()
+
+db.dropDatabase()
+
